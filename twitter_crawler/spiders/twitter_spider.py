a30ec08758a7ddea3de839b97cf0ab49a3d0852b
jaki2012 <jaki2012@sina.cn>
2018-04-14 22:27:52 +0800

    now we can read keywords from txt

--- twitter_crawler/spiders/twitter_spider.py
+++ twitter_crawler/spiders/twitter_spider.py
@@ -3,6 +3,7 @@ from scrapy import http
 from scrapy.selector import Selector
 from datetime import datetime
 import json
+import os
 
 from twitter_crawler.items import TwitterCrawlerItem
 
@@ -10,19 +11,61 @@ class TwitterSpider(scrapy.Spider):
 	
 	name = "twitter"
 
-	start_urls = ['https://twitter.com/i/search/timeline?f=tweets&q=hello&src=typed']
+	def __init__(self, query=None, *args ,**kwargs):
 
-	def __init__(self):
+		super(TwitterSpider, self).__init__(*args, **kwargs)
+
+		self.url_base = 'https://twitter.com/i/search/timeline?f=tweets&src=typd&q={}'
+		self.start_urls = []
+		self.queries = []
+		self.temp_query = None
+
+		if kwargs.get('file') is not None:
+			filename = os.path.join(os.getcwd(), kwargs.get('file'))
+			with open(filename, 'r') as file:
+				keywords = file.readline().strip().split(',')
+				if len(keywords) == 0:
+					print("No query word specified, please check your command.")
+				else:
+					self.start_urls = []
+					for keyword in keywords:
+						self.start_urls.append(self.construct_url(keyword, **kwargs))
+						self.queries.append(self.temp_query)
+		else:
+			if query is None:
+				print("No query word specified, please check your command.")
+			else:
+				self.start_urls = [self.construct_url(query, **kwargs)]
+				self.queries.append(self.temp_query)
+
 		self.currentIteration = 0
-		self.totalIterations = 5
+		self.totalIterations = 100
 		self.min_tweet = None
+
+		# To verify if each start_url corresponds to its original query
+		for i, start_url in enumerate(self.start_urls):
+			print("%s with %s" % (self.queries[i], start_url))
+
+	def start_requests(self):
+		for i, url in enumerate(self.start_urls):
+			yield http.Request(url, dont_filter=True, meta={"query": self.queries[i]})
+
+	def construct_url(self, query, **kwargs):
+		constructed_url = self.url_base
+		if kwargs.get("since") is not None:
+			query += (" since:" + kwargs.get("since"))
+		constructed_url  = self.url_base.format(query)
+		if kwargs.get('language') is not None:
+				constructed_url  += ("&l=" + kwargs.get("language"))
+		self.temp_query = query
+		return constructed_url
 
 	def parse(self, response):
 
 		data = json.loads(response.body.decode("utf-8"))
-		response = Selector(text=data['items_html'])
+		response_selec = Selector(text=data['items_html'])
 		# sels = response.xpath('.//div[@class="stream"]/ol[contains(@class, "stream-items")]/li[contains(@class, "stream-item")]')
-		sels = response.xpath('//li[@data-item-type="tweet"]/div')
+		sels = response_selec.xpath('//li[@data-item-type="tweet"]/div')
 		len_sels = len(sels)
 		for i, sel in enumerate(sels):
 
@@ -43,7 +86,7 @@ class TwitterSpider(scrapy.Spider):
 			item['replies'] = int(sel.css('span.ProfileTweet-action--reply > span.ProfileTweet-actionCount')
 				.xpath('@data-tweet-stat-count').extract()[0])
 
-			# multimedias
+			# multimedia properties
 			urls = []
 			url_nodes = sel.css('a.twitter-timeline-link:not(.u-hidden)')
 			for url_node in url_nodes:
@@ -92,8 +135,9 @@ class TwitterSpider(scrapy.Spider):
 				else:
 					max_position = "TWEET-%s-%s" % (max_tweet['tweetId'], self.min_tweet['tweetId'])
 
-				print("Search hints9 %d" %self.currentIteration)
+				print("Search hints13 %d" %self.currentIteration)
 				# data_min_position = response.xpath('.//div[@id="timeline"]//div[contains(@class, "stream-container")]/@data-min-position').extract()[0]
 				# max_position = data_min_position.split('-')[-1]
-				next_url = "https://twitter.com/i/search/timeline?f=tweets&q=hello&src=typd&max_position=" + max_position+ "&reset_error_state=false"
-				yield http.Request(next_url, callback=self.parse)
+				next_url = "https://twitter.com/i/search/timeline?f=tweets&q={}&src=typd&max_position=" + max_position+ "&reset_error_state=false"
+				next_url = next_url.format(response.meta['query'])
+				yield http.Request(next_url, callback=self.parse, meta={"query": response.meta['query']})
